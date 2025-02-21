308a2b97c28295d6d6dd65ee6ef590094625c822
Viktoria <avramovaviki017@gmail.com>
2025-02-22 00:30:48 +0200

    removed unused statemant

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Student.Student;
 import Student.StudentAdv;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
