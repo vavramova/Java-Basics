49ab1a64c2295d2204810c4c4b2440781cd85619
Viktoria <avramovaviki017@gmail.com>
2025-02-21 20:03:04 +0200

    made grade private and added description

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -4,26 +4,41 @@ import java.util.Random;
 
 public class Student {
     private final String name;
-    private int grade;
+    private final int grade;
 
     public Student(String name){
         this.name = name;
         this.grade = generateRandomGrade();
     }
 
+    /**
+     * a methode that generates a random grade for every student
+     * @return the grade
+     */
     private int generateRandomGrade(){
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
 
+    /**
+     * a methode that gets the name of a student
+     * @return the name
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * a methode that gives the grade of a student
+     * @return the grade
+     */
     public int getGrade(){
         return grade;
     }
 
+    /**
+     * a methode that helps to print information about a student
+     */
     public void printInfo(){
         System.out.println(name + " : " + grade);
     }
